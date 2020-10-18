d73430e9fe6d9250470c60261ac5d23a68e5363b
Edgar Huerta M <ed.huerta.m@gmail.com>
2020-10-18 12:12:46 -0500

    Configuractión MediatR

--- aplicacion/Cursos/Consulta.cs
+++ aplicacion/Cursos/Consulta.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Dominio;
+using Microsoft.EntityFrameworkCore;
+using Persistencia;
 using MediatR;
 
 namespace aplicacion.Cursos
@@ -11,10 +13,28 @@ namespace aplicacion.Cursos
         public class ListaCursos : IRequest<List<Curso>> { }
         public class Manejador : IRequestHandler<ListaCursos, List<Curso>>
         {
-            private readonly Curso
+            private readonly CursosOnlinecontext _context;
+            public Manejador(CursosOnlinecontext context)
+            {
+                _context = context;
+            }
+            public async Task<List<Curso>> Handle(ListaCursos request, CancellationToken cancellationToken)
+            {
+                //throw new System.NotImplementedException();
+                var cursos = await _context.Curso.ToListAsync();
+                return cursos;
+            }
+
 
         }
 
     }
 
+    public interface IRequest<T>
+    {
+    }
+
+    public interface IRequestHandler<T1, T2>
+    {
+    }
 }
